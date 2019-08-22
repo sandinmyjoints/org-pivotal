83af0bf8ecd220bb9b4d666e8950046ecc9fb21d
Huy Duong <huy.duong@employmenthero.com>
2019-08-22 22:08:34 +0700

    Add test for org-pivotal-install-project-metadata
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- tests/test-org-pivotal.el
+++ tests/test-org-pivotal.el
@@ -44,7 +44,42 @@
       (expect 'completing-read :to-have-been-called-with "Select your project?" '("Test project 1" "Test project 2")))
 
     (it "returns correct project_id"
-      (expect project-id :to-equal 12345678))))
+      (expect project-id :to-equal 12345678)))
+
+  (describe "org-pivotal-install-project-metadata"
+    :var (my-info)
+    (before-each
+      (setq my-info '((projects . [((project_id . 12345678)
+                                    (project_name . "Test project 1"))
+                                   ((project_id . 87654321)
+                                    (project_name . "Test project 2"))])
+                      (name . "Huy Duong")
+                      (age . "18")))
+      (spy-on 'org-pivotal-api--get-my-info :and-return-value my-info)
+      (spy-on 'org-pivotal--select-project :and-return-value 12345678)
+      (spy-on 'org-pivotal-api--get-project-info
+              :and-return-value '((project_id . 12345678)
+                                  (project_name . "Test project 1")))
+      (spy-on 'org-pivotal--update-buffer-with-metadata)
+      (org-pivotal-install-project-metadata))
+
+    (it "calls API to get user info"
+      (expect 'org-pivotal-api--get-my-info :to-have-been-called))
+
+    (it "informs user to select project"
+      (expect 'org-pivotal--select-project
+              :to-have-been-called-with
+              (alist-get 'projects my-info)))
+
+    (it "calls API to get project info"
+      (expect 'org-pivotal-api--get-project-info :to-have-been-called-with 12345678))
+
+    (it "updates buffer with "
+      (expect 'org-pivotal--update-buffer-with-metadata
+              :to-have-been-called-with
+              '((project_id . 12345678)
+                (project_name . "Test project 1"))
+              my-info))))
 
 (provide 'test-org-pivotal)
 
